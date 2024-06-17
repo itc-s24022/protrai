8b3107be451d8337fa55d29f47e3c4439af56045
s24022 <s24022@std.it-college.ac.jp>
2024-06-17 06:18:53 +0000

    fast commit

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-for _ in range(+1):
-    i = input("Enter a value: ")
-print(i)
+n = int(input("入力したい回数を入力してください: "))
+last_value = ""
+for i in range(n):
+    last_value = input(f"{i + 1} 番目の値を入力してください: ")
+print(f"最後に入力された値は: {last_value}")
