f5899cba774704b71ca1ed18787d6ccc96e6edcc
s24022 <s24022@sdt.it-college.ac.jp>
2024-06-17 05:26:11 +0000

    fast commit

--- for.py
+++ for.py
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 for i in range(1, 31):
     print("s240" + str(i).zfill(2))
+=======
+for i in range(31):
+    print("s240" + str(i).zfill(2))
+>>>>>>> d4a5a63 (fast commit)
---
+++ test.py
@@ -0,0 +1,3 @@
+for _ in range(+1):
+    i = input("Enter a value: ")
+print(i)
