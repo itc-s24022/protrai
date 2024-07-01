0c987d13f90f5115a8ffe4ce3a36e8c3c6808924
itc-s24022 <167942819+itc-s24022@users.noreply.github.com>
2024-07-01 16:40:24 +0900

    PIL

---
+++ dispimage Kadai2.py
@@ -0,0 +1,30 @@
+#s24022
+#PLIの授業課題
+
+import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image, PIL.ImageTk
+def dispPhoto(path):
+    newImage = PIL.Image.open(path).convert("L").rotate(90).transpose(PIL.Image.FLIP_LEFT_RIGHT).resize((300,300))
+    imageData = PIL.ImageTk.PhotoImage(newImage)
+    imageLabel.configure(image = imageData)
+    imageLabel.image = imageData
+    lbl2 = tk.Label(text=path,font=("Helvertica",16))
+    lbl2.pack()
+
+def openFile():
+    fpath = fd.askopenfilename()
+    if fpath:
+        dispPhoto(fpath)
+        print(fpath)
+        lbl3 = tk.Label(text=fpath)
+        lbl3.pack()
+root = tk.Tk()
+root.geometry("400x400")
+lbl2 = tk.Label(text=":art:画像表示アプリ バージョン2.0:art:")
+btn = tk.Button(text="ファイルを開く", command = openFile)
+imageLabel = tk.Label()
+lbl2.pack()
+btn.pack()
+imageLabel.pack()
+tk.mainloop()
